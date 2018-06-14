506fee69806b3c7f6978a19ce37e2964fbdaae30
Ilya Meerovich <ilya.meerov@gmail.com>
2018-06-13 19:12:12 -0700

    Cleaned up front page

--- footer.php
+++ footer.php
@@ -15,10 +15,7 @@
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
-<<<<<<< HEAD
 			<!-- <script type="text/javascript" src="/caravel/wp-content/themes/caraveltheme/lib/jquery.visible.min.js"></script> -->
-=======
->>>>>>> acaf7b802cc12b50fbb6b3bb049dc85981e75557
        <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0-beta/js/materialize.min.js"></script> -->
 		<?php wp_footer(); ?>
 
--- js/main.js
+++ js/main.js
@@ -1,15 +1,7 @@
 jQuery(document).ready(function ($) {
-<<<<<<< HEAD
-  
-  var homeFlag = document.querySelector('.header-container');
-  console.log(homeFlag.visible());
-
-
-=======
 
   var homeFlag = document.querySelector('#header-container').id;
   console.log(homeFlag);
->>>>>>> 0ce8fb9ef32d48cf88c002b73c30a3a5c062d594
 
 
   if (!!homeFlag && homeFlag) {
