e4063df7f3d5a17f76c6cd8360228fb6137b00f8
NCMoseley <ncmoseley@gmail.com>
2018-06-15 10:44:59 -0700

    Add jquery.unveil.js

--- functions.php
+++ functions.php
@@ -99,6 +99,8 @@ function red_starter_scripts()
 
     wp_enqueue_script('fullpage-js', get_template_directory_uri() . '/lib/jquery.fullpage.js', array(jquery), '20130115', true);
     wp_enqueue_script('fullpage-extensions-js', get_template_directory_uri() . '/lib/jquery.fullpage.extensions.min.js', array(jQuery), '20130115', true);
+
+    wp_enqueue_script('jquery-unveil', get_template_directory_uri() . '/build/js/jquery.unveil.min.js', array('jquery'), '', true);
     
     wp_enqueue_script('waypoints', get_template_directory_uri() . '/lib/noframework.waypoints.min.js', array(), '20130115', true);
     
