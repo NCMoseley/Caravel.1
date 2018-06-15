55ac7ec6700d3f1ef30a696b0cb46630549db32a
NCMoseley <ncmoseley@gmail.com>
2018-06-15 11:43:48 -0700

    Add jpg images for carousel

--- template-parts/carousel.php
+++ template-parts/carousel.php
@@ -21,7 +21,7 @@
             <div class="tile">
               
               <div class="tile__media">
-                <img class="tile__img" alt="hummingbird-photo" src="Hummingbird-Blur.jpg" data-src="<?php echo get_template_directory_uri() . '/assets/Hummingbird.png'?>"
+                <img class="tile__img" alt="hummingbird-photo" src="<?php echo get_template_directory_uri() . '/assets/Hummingbird-Blur.jpg'?>" data-src="<?php echo get_template_directory_uri() . '/assets/Hummingbird.jpg'?>"
                 />
               </div>
              <a href="https://volunteer.hummingbirdnetwork.com" target="_blank">
@@ -35,7 +35,7 @@
           
              <div class="tile">
               <div class="tile__media">
-                <img class="tile__img" alt="packwest-photo" src="PacWest-Blur.jpg" data-src="<?php echo get_template_directory_uri() . '/assets/PacWest.png'?>" />
+                <img class="tile__img" alt="packwest-photo" src="<?php echo get_template_directory_uri() . '/assets/PacWest-Blur.jpg'?>" data-src="<?php echo get_template_directory_uri() . '/assets/PacWest.jpg'?>" />
               </div>
               <a href="https://pacwestcanopy.com/" target="_blank">
               <div class="tile__details">
@@ -48,7 +48,7 @@
           
             <div class="tile">
               <div class="tile__media">
-                <img class="tile__img" alt="valo-photo" src="Valo-Blur.jpg" data-src="<?php echo get_template_directory_uri() . '/assets/Valo.png'?>" />
+                <img class="tile__img" alt="valo-photo" src="<?php echo get_template_directory_uri() . '/assets/Valo-Blur.jpg'?>" data-src="<?php echo get_template_directory_uri() . '/assets/Valo.jpg'?>" />
               </div>
               <a href="https://valosmartcity.com/" target="_blank">
               <div class="tile__details">
@@ -61,7 +61,7 @@
 
             <div class="tile">
               <div class="tile__media">
-                <img class="tile__img" alt="nexus-photo" src="Nexus-Blur.jpg" data-src="<?php echo get_template_directory_uri() . '/assets/Nexus.png'?>" />
+                <img class="tile__img" alt="nexus-photo" src="<?php echo get_template_directory_uri() . '/assets/Nexus-Blur.jpg'?>" data-src="<?php echo get_template_directory_uri() . '/assets/Nexus.jpg'?>" />
               </div>
               <a href="http://nexus.van.cp.academy.red/" target="_blank">
               <div class="tile__details">
