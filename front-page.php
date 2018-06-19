4688ec0ca22620c830e1352f499248947cd46002
NCMoseley <ncmoseley@gmail.com>
2018-06-19 14:42:35 -0700

    Add logic for email form

--- front-page.php
+++ front-page.php
@@ -82,10 +82,91 @@ get_header(); ?>
 									<div class="main clearfix">
 
 										<?php
+    if (isset($_POST['email']) == true) {
+        $displayInput = false;
+
+        
+        $to = $_POST['email'];
+        $subject = "Submit was a Success!";
+
+        $message = '
+        <html>
+        <head>
+        <title>ETS</title>
+        </head>';
+        $message .=
+        
+    '<body style="margin: 0; padding: 0; bgcolor: #eeeeee ">
+        <table border="0" cellpadding="0" cellspacing="0" width="100%">
+         <tr>
+            <td style="padding: 20px 0 30px 0;">
+              <table align="center" border="0" cellpadding="0" cellspacing="0" width="600" style="border: 1px solid #cccccc;">
+                  <tr>
+                    <td align="center" bgcolor="#b5d3e7" style="padding: 40px 0 30px 0;">
+                      <img src="" alt="Creating Email Magic" width="450" height="80" style="display: block;" />
+                    </td>
+                  </tr>
+                  <tr>
+                    <td bgcolor="#f8f8f8" style="padding: 40px 30px 40px 30px;">
+                      <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                       <tr>
+                        <td>
+                            <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                              <tr>
+                                <td style="color: #ecb42b; font-family: Helvetica, Arial, Verdana, "Trebuchet MS"; font-size: 24px;">
+                                  <b>You have successfully registered!</b>
+                                </td>
+                              </tr>
+                              <tr>
+                               <td style="padding: 20px 0 30px 0; color: #ecb42b; font-family: Helvetica, Arial, Verdana, "Trebuchet MS"; font-size: 16px; line-height: 20px;">
+                                We are just putting the finishing touches on the magazine. We appreciate your support, and will send the magazine as soon as it is complete.
+                               </td>
+                              </tr>
+                            </table>
+                          </td>
+                       </tr>
+                      </table>
+                    </td>               
+                  </tr>
+                  <tr>
+                    <td bgcolor="#b5d3e7" style="padding: 30px 30px 30px 30px;">
+                      <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                        <tr>
+                          <td style="color: #001d48; font-family: Helvetica, Arial, Verdana, "Trebuchet MS"; font-size: 16px; line-height: 20px;">
+                          &copy;  2018. All Rights Reserved.
+                          </td>
+                        </tr>
+                      </table>
+                    </td>
+                  </tr>
+                </table>        
+          </td>
+         </tr>
+        </table>
+       </body>
+      
+       ';
+
+        // Always set content-type when sending HTML email
+        $headers = "MIME-Version: 1.0" . "\r\n";
+        $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
+
+        // More headers
+        $headers .= 'From: <ncmoseley@gmail.com>' . "\r\n";
+        mail($to, $subject, $message, $headers);
+        $toMe = "ncmoseley@gmail.com";
+        $subjectMine = "Someone Submitted Contact form";
+        $txt = $_POST['email'] . " just submitted";
+        $headersMine = "From: webmaster@example.com";
+        mail($toMe, $subjectMine, $txt, $headersMine);
+    } else {
+        $displayInput = true;
+    }
+?>
+
+										<?php
                 if ($displayInput == true) {
                     ?>
-
-
 										<form id="nl-form" class="nl-form" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
 											I want to get my website or app
 											<select>
@@ -108,6 +189,7 @@ get_header(); ?>
 											<input type="text" value="" placeholder="any time" data-subline="For example: <em>next-week</em> or <em>next-month</em>" /> in
 											<input type="text" value="" placeholder="any city" data-subline="For example: <em>Los Angeles</em> or <em>Vancouver</em>"
 											/>
+											<input type="email" placeholder="  E-MAIL" id="myEmail" required="required" name="email" class="userInput"><input type="image" name="submit" src="img/send.png" alt="Submit" class="mail"/>
 											<div class="nl-submit-wrap">
 												<button class="nl-submit" type="submit">Submit</button>
 											</div>
@@ -147,15 +229,17 @@ get_header(); ?>
 
 	<!-- <?php get_template_part('template-parts/content-form'); ?> -->
 
+	
+
 
 
     
-        <div class="left">
+        <!-- <div class="left">
             <?php
                 if ($displayInput == true) {
                     ?>
 
-            <form action="<?php echo $_SERVER['template-parts/content-form']; ?>" method="post">
+            <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
             <div class="mimo">
             <input type="email" placeholder="  E-MAIL" id="myEmail" required="required" name="email" class="userInput"><input type="image" name="submit" src="img/send.png" alt="Submit" class="mail"/>
             </div>
@@ -168,7 +252,7 @@ get_header(); ?>
             <?php
                 }
             ?>
-        </div>
+        </div> -->
         
         
    
